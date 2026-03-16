87d8b6b6783cf8e8cc488ce0095f0200021dd0a3
duydoanb <duy.doan@agest.vn>
2026-03-16 15:16:56 +0700

    just add skeleton

---
+++ tests/purchase/TC_07.spec.ts
@@ -0,0 +1,14 @@
+import { BillingInfo } from '../../data-objects/billingInfo';
+import { BillingInfoEnum, MenuTab, ProductViewMode } from '../../data-objects/dataEnums';
+import { test } from '../../fixtures/beforeAndAfterTest';
+import { CheckoutPage } from '../../pages/checkoutPage';
+import { HomePage } from '../../pages/homePage';
+import { MyCartPage } from '../../pages/myCartPage';
+import { OrderStatusPage } from '../../pages/orderStatusPage';
+import { ProductPage } from '../../pages/productPage';
+import { DataUtils } from '../../utils/utilities';
+import { ProductData } from '../../data-objects/productData';
+
+test('TC 07: Ensure proper error handling when mandatory fields are blank', async ({ page }) => {
+
+});
